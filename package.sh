5d1a8a066684e8597b1a8ced6c4884b2c3daca5b
Guilhem Vellut <g@vellut.com>
2019-04-01 11:22:46 +0200

    Updated version before release

--- package.sh
+++ package.sh
@@ -1,3 +1,4 @@
+rm -f fhrgr.zip
 cd ..
-zip -r FreehandRasterGeoreferencer/fhrgr.zip FreehandRasterGeoreferencer -x ".*" -x "*/.*" -x "*.pyc" 
+zip -r FreehandRasterGeoreferencer/fhrgr.zip FreehandRasterGeoreferencer -x ".*" -x "*/.*" -x "*.pyc" -x "*/__pycache__/*" -x "*.sh" -x "*.bat"
 cd FreehandRasterGeoreferencer
